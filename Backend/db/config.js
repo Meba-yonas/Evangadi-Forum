e8d3f210a73d51b61b8430161311d9b09e218369
Mebayonas Yonas <mebayonasyonas@gmail.com>
2024-11-13 21:04:09 +0300

    the last one

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -1,10 +1,10 @@
 const mysql2 = require("mysql2");
 
 const dbconnection = mysql2.createPool({
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
+  user:'u0loh2tpa54m5qvt',
+  database:"brpzlcvc5xbkfzxsrk1f",
+  host:"brpzlcvc5xbkfzxsrk1f-mysql.services.clever-cloud.com",
+  password:"LFzaB65sUbPt2omicrav",
   connectionLimit: 10,
 });
 
